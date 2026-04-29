028b9e021b493151659370050a3f9bf95aed15df
Roldan Abaloyan <roldanabaloyan3@gmail.com>
2026-04-29 20:30:16 +0800

    fix db.php

--- Dashboard/db.php
+++ Dashboard/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "skyph");
+$conn = new mysqli("localhost", "root", "", "sky-ph");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
